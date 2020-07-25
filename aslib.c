0675128febba6960bb73b718e3f28068d498d569
Anil Madhavapeddy <anil@recoil.org>
2020-07-25 20:02:52 +0100

    fix the racket one, ctors are the problem

--- aslib.c
+++ aslib.c
@@ -2,11 +2,16 @@
 #include <caml/mlvalues.h>
 #include <caml/callback.h>
 
+#if 0
 __attribute__ ((__constructor__))
 void init(void) {
   char *caml_argv[1] = { NULL };
   caml_startup(caml_argv);
+  return;
 }
+#endif
+
+static int caml_started_up;
 
 int call_caml_doubler(int arg)
 {
@@ -16,5 +21,10 @@ int call_caml_doubler(int arg)
 }
 
 int double_int(int a) {
+  char *caml_argv[1] = { NULL };
+  if (!caml_started_up) {
+    caml_startup(caml_argv);
+    caml_started_up = 1;
+  }
   return call_caml_doubler(a);
 }
